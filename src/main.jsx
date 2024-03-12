d06f471a20babdf8050356be63696af2cdbfc8ca
Tharit Lertpakdeewong <tharit.tlk@gmail.com>
2024-03-13 00:28:53 +0700

    deleteUser

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,18 @@
-import './index.css'
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import { BrowserRouter } from 'react-router-dom'
-
+import "./index.css";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import store from "./store";
 
 // import store from './store'
 // import { Provider } from 'react-redux'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-    < App />
-  </BrowserRouter>
-  
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>
+);
--- src/pages/UserList.jsx
+++ src/pages/UserList.jsx
@@ -1,7 +1,42 @@
+import { useEffect } from 'react'
+import { Link } from 'react-router-dom'
+import { useSelector, useDispatch } from 'react-redux'
+import { fetchUsers, deleteUser } from '../actions/userActions'
+
 const UserList = () => {
+    const dispatch = useDispatch()
+    const users = useSelector((state) => state.user.users)
+
+    useEffect(() => {
+        dispatch(fetchUsers())
+    }, [dispatch])
+    
+
   return (
-    <div>
-      <p className="text-red-500 text-3xl"> User List c</p>
+    <div className="container mx-auto p-4">
+      <h3 className="mb-4 text-lg font-semibold">User List</h3>
+      {users.map((user) => (
+        <div
+          key={user.id}
+          className="flex items-center justify-between border-b py-2"
+        >
+          <div>
+            <p>Name: {user.name}</p>
+            <p>Email: {user.email}</p>
+            <p>Phone: {user.phoneNumber}</p>
+          </div>
+          <div>
+            <button onClick={() => dispatch(deleteUser(user.id))} className="mr-2 rounded bg-red-500 px-3 py-1 text-white">
+              Delete
+            </button>
+            <Link to={`/edit/${user.id}`}>
+              <button className="rounded bg-blue-500 px-3 py-1 text-white">
+                Edit
+              </button>
+            </Link>
+          </div>
+        </div>
+      ))}
     </div>
   );
 };
